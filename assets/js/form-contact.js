2fcfc01c5bc88e50eea0a89737d0daf1aadda88c
Alejandro <silveyra.92@gmail.com>
2017-12-30 19:43:54 -0600

    cleans up icons

--- assets/js/form-contact.js
+++ assets/js/form-contact.js
@@ -4,30 +4,30 @@ $(document).ready(function() {
 	$('form#contact-form').submit(function() {
 	$('form#contact-form .error').remove();
 	var hasError = false;
-	
-	// $('.requiredField').each(function() {
-		// if(jQuery.trim($(this).val()) == '') {
-			// var labelText = $(this).prev('label').text();
-			// $(this).parent().append('<span class="error">You forgot to enter your '+labelText+'</span>');
-			// $(this).addClass('inputError');
-			// hasError = true;
-		// }
-		// else if($(this).hasClass('email')) {
-		// var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-		// if(!emailReg.test(jQuery.trim($(this).val()))) {
-			// var labelText = $(this).prev('label').text();
-			// $(this).parent().append('<span class="error">You entered an invalid '+labelText+'</span>');
-			// $(this).addClass('inputError');
-			// hasError = true;
-			// }
-		// }
-    // });
-	
+
+	$('.requiredField').each(function() {
+		if(jQuery.trim($(this).val()) == '') {
+			var labelText = $(this).prev('label').text();
+			$(this).parent().append('<span class="error">You forgot to enter your '+labelText+'</span>');
+			$(this).addClass('inputError');
+			hasError = true;
+		}
+		else if($(this).hasClass('email')) {
+		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+		if(!emailReg.test(jQuery.trim($(this).val()))) {
+			var labelText = $(this).prev('label').text();
+			$(this).parent().append('<span class="error">You entered an invalid '+labelText+'</span>');
+			$(this).addClass('inputError');
+			hasError = true;
+			}
+		}
+    });
+
     if(!hasError) {
-		// $('form#contact-form input.submit').fadeOut('normal', function() {
-			// $(this).parent().append('');
-		// });
-		
+		$('form#contact-form input.submit').fadeOut('normal', function() {
+			$(this).parent().append('');
+		});
+
 		$("#loader").show();
 		$.ajax({
 			url: "contact.php",
@@ -42,9 +42,9 @@ $(document).ready(function() {
 			  $("#loader").hide();
 			  })
 			}
-		});	   
+		});
 	return false;
     }
- 
+
    });
 });
